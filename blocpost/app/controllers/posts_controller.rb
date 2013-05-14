af8e109583482a4c56ff4874128e26b574d5476c
Hakim Benbekhti <hakim.benbekhti@gmail.com>
2013-05-14 21:42:12 +0100

    first commit

---
+++ blocpost/app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  
+  def index
+      @posts = Post.all
+	  
+	  respond_to do |format|
+       format.html # index.html.erb
+       format.json { render json: @posts }
+	  end
+  end
+
+  def new
+      @posts = Post.new
+	  
+	  respond_to do |format|
+	   format.html
+	   format.json { render :json => @post }
+	  end
+  end
+
+  def show
+      @post = Post.find(params[:id])
+
+      respond_to do |format|
+       format.html # show.html.erb
+       format.json { render json: @post }
+      end
+  end
+
+  def edit
+      @post = Post.find(params [:id])
+  end
+end
---
+++ blocpost/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocpost::Application.config.secret_token = '1a8ec5f061e1f1490d3b854813c5d5f1552d41aa2db12f0337aedb683f4e1912f8d71b2c342685ad4538177a2b86fa562e05fa449b67a96fbcb63a030e826147'
